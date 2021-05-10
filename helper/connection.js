68b5aa51e4fcbef9aa3b73030089b6f488dbb055
Sebastian <19Sebastian95@gmx.de>
2021-05-10 23:59:48 +0200

    update connection

--- helper/connection.js
+++ helper/connection.js
@@ -1,3 +1,16 @@
-export default function doRequest(host, input, vars) {
+import axios from 'axios';
+
+export default async function doRequest(host, input, vars) {
+    console.log("Called!");
     console.log(host, input, vars);
+    const tld = (input.protocol ? input.protocol.toLowerCase() : "http") + "://" + (host ? host : "localhost");
+    const url = tld;
+
+    let query = await axios({
+        method: input.method,
+        url: url
+    });
+    console.log(query);
+
+    return query;
 }
